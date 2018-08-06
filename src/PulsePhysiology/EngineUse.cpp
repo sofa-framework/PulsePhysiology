5a6fbd42220439e703fff10fe249618a87b06a1c
Manan <jn.manan25@gmail.com>
2018-08-06 14:27:02 +0530

    Fixes Logger and EventHandler settings

--- src/PulsePhysiology/EngineUse.cpp
+++ src/PulsePhysiology/EngineUse.cpp
@@ -33,17 +33,6 @@
 ///
 /// \details
 /// The data model has a logging capability that the engine utilizes to log any info to a log file.
-///
-/// This logger has different levels at which it can log events:
-///
-///     Debug   - Extremely low level information, intended for the developer to assess any questions in functionality
-///     Info    - A general level of information where the engine wants to inform a user of something
-///     Warning - Something was not input properly or an assumption was made in the underlying methodology
-///     Error   - A recoverable error has occured and the engine is still able to proceed, but results should be inspected for correctness
-///     Fatal   - A unrcoverable error has occured and the engine will stop calculating
-///
-/// The user can create custom forwarders and add them to the engine's logger so that the forwarding methods are called
-/// when events are logged. This allows a calling program to be notified of logging messages in order to handle them as needed.
 //--------------------------------------------------------------------------------------------------
 class MyLogger : public LoggerForward
 {
@@ -58,18 +47,7 @@ public:
 //--------------------------------------------------------------------------------------------------
 /// \brief
 /// A class used to forward event activity for application specific handling logic
-///
-/// \details
-/// Various objects can enter into and out of a particular state, this will let you know when that happens.
-///
-/// Currently there are two objects that have event based data
-///
-///     Patient - please see the Patient.xsd
-///     AnesthesiaMachine - please see Anesthesia.xsd
-///
-/// The time provided is the current engine time.
-/// The provided methods will be called at the end of the time step when the event was triggered
-/// This allows a calling program to be notified of events in order to handle them as needed.
+
 //--------------------------------------------------------------------------------------------------
 class MyEventHandler : public SEEventHandler
 {
@@ -92,43 +70,29 @@ public:
 void HowToEngineUse()
 {
   // Create an engine object
-  // PulseEngines will always output log messages to stdout and a log file  
-  // If you want this engine to write a log file, include the name 
-  // of the log file. If nullptr is given, the engine will only output to the console
+
   std::unique_ptr<PhysiologyEngine> pe = CreatePulseEngine("HowToEngineUse.log");
   pe->GetLogger()->Info("HowToEngineUse");
 
-  // This PulseEngine logger is based on log4cpp (which is based on log4j)
-  // PulseEngine logs to several distinct, ordered
-  // category levels: DEBUG, INFO, WARN, ERROR, FATAL
-  // These categories are orders, if your level is set to DEBUG you will recieve ALL messages.
-  // If set to INFO, you will not recieve DEBUG, but everything else
-  // If set to WARN, you will not recieve DEBUG and INFO, but everything else
-  // You can specify which level you would like the engine to log
-  // By Default the LogLevel is INFO
-  // pe->GetLogger()->SetLogLevel(log4cpp::Priority::INFO);
 
   // You can tell the PulseEngine to also direct any output
   // to your own function. For example if you want to capture and process messages
   // and handle them in your own way, Give PulseEngine an instance of the LoggerForward class
   // and PulseEngine will log and also call the provided method with the message.
   // You can specify a set of functions to be called for any one of the log levels
-  MyLogger myLogger;
-  pe->GetLogger()->SetForward(&myLogger);
+  //MyLogger myLogger;
+  //pe->GetLogger()->SetForward(&myLogger);
 
   // You can tell the PulseEngine to also notify you of any events as well
   // You can Poll objects for events, for example
   // pe->GetPatient().
 
-  // NOTE, setting the LogLevel, Forwarder, EventHandler can be done after initialize and as many times as you want
-
   // The first order of business is to initialize the engine by loading a patient state.
   // Patient states provided in the SDK are the state of the engine at the time they stabilize
   // More details on creating a patient and stabilizing the engine can be found in HowTo-CreateAPatient.cpp
   SEScalarTime startTime;
-  // You can optionally specify a specific simulation time for the engine to use as its initial simulation time
-  // If no time is provided, the simulation time that is in the state file will be used
-  // Note the provided state files are named to include what is simulation time is
+
+
   startTime.SetValue(0, TimeUnit::s);
   if (!pe->LoadStateFile("./states/StandardMale@0s.pba", &startTime))
   {
@@ -141,14 +105,9 @@ void HowToEngineUse()
   // You can either poll/query the patient object to see if it is in a specific state
   pe->GetPatient().IsEventActive(cdm::ePatient_Event_CardiacArrest);
   // You can also derive a callback class that will be called whenever an Event is entered or exited by the patient
-  MyEventHandler myEventHandler;
-  pe->SetEventHandler(&myEventHandler);
+  //MyEventHandler myEventHandler;
+  //pe->SetEventHandler(&myEventHandler);
 
-  // We are interested in 2 substances and their amounts in compartments
-  // Let's grab the substance via name from the substance manager, string look ups are slow
-  // So it's a good idea to cache this pointer so you can reuse it when asking at various time steps
-  // You can find all substances defined in pba files in the substances directory of your working directory
-  // Names are in those pba files. ALL substance pba files are loaded into a substance managers when the engine is created
   SESubstance* O2 = pe->GetSubstanceManager().GetSubstance("Oxygen");
   SESubstance* CO2 = pe->GetSubstanceManager().GetSubstance("CarbonDioxide");
 
@@ -181,17 +140,6 @@ void HowToEngineUse()
   // pe->AdvanceModelTime(5,TimeUnit::s);
   // If you do not provide a time, the engine will execute 1 time step, the Pulse timestep can be retrieved with:
   //pe->GetTimeStep(TimeUnit::s);
-
-  // You will notice that the timing calls mentioned above take in a specific unit
-  // The Common Data Model has a conversion library internal to it that will convert data to whatever you specify.
-  // There are predefined units defined in each unit quantity scalar class header.
-  // Note the SE simpley stands for Synthetic Enviroment.
-  // Each unit quantity has it's own class representation along with commonly used unit definitions
-  // You may use strings to define your unit if you do not find a unit predefined (but strings have a runtime cost to them)
-  // Units are defined in the standard abbreviation, for example:
-  // http://www.bipm.org/en/si/
-  // Along with si units, english units and medical units are supported
-  // You can check to see if a unit is valid by calling the IsValidUnit method on a particular quantity scalar class.
   
   // Engine Data is available in 3 general forms
   // Assessments - Data formed at a level of a clinicians report. 
@@ -205,24 +153,6 @@ void HowToEngineUse()
   //                Compartments are contained in a hierarchical object model that  encapsulates the body and its anatomy down to organs and organ components
   //                For example the lung compartment is made up of the left lung and right lung
 
-  // When retrieving one of these types of data structures, the structure will contain various properties of a SEScalar types, std::string and enums.
-  // (ex. HeartRate, Volume, Flow, ResperatoryRate, etc.)
-  // The general naming convention used in creating these structures and their properties is a 'Get', 'Has' style
-  // For Scalar objects, these structures only instantiate scalar objects for data they have, in order to maintain a small memory footprint.
-  // Calling the Get method will create A new scalar (managed by the structure) and the value will be set to NaN. 
-  // ex. pe->GetCardiovascularSystem()->GetBloodVolume()->GetValue(VolumeUnit::L);
-  // You can check to see if an engine has output a particular property by calling a complimentary 'Has' method
-  // ex. pe->GetCardiovascularSystem()->HasBloodVolume(), if the value is not NaN, true will be returned
-  //
-  // When you set a value to a scalar, you MUST provide a unit!
-  // When you which to get the value of a scalar, you MUST provide a unit! 
-  // If the scalar is of a unitless quantity (i.e. fractions) you can use SEScalar::unitless or SEScalar::nullUnit
-  // Scalars hold the data in the unit provided, conversions are only performed if the requested unit is not the same as the Scalars set unit
-  // Conversions are fast, but it is recommended to understand the units used in the engine and try to use those for optimal use of the engine
-  // 
-  // SEScalar has overloaded the << operator and will out put in the form of 'value(unit)' (ex. '50(mL)')
-  // If the scalar is unitless no parens will be provided.
-
   // Here we demostrate pulling data from various physiology systems
   
   pe->GetLogger()->Info(std::stringstream() << "HeartRate : " << pe->GetCardiovascularSystem()->GetHeartRate(FrequencyUnit::Per_min) << "bpm");
@@ -232,10 +162,7 @@ void HowToEngineUse()
   pe->GetLogger()->Info(std::stringstream() << "MeanArterialPressure : " << pe->GetCardiovascularSystem()->GetMeanArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
 
   // Here we demonstrate pulling data from a compartment
-  // NOTE THAT WHEN YOU PULL A COMPARTMENT FROM THE COMPARTMENT MANAGER, THAT IS A STRING LOOK UP, TRY TO CACHE COMPARTMENTS OF INTEREST INTO YOUR OWN LIST/MAP
-  // This allows a more direct access to the underlying data calculated by the methodology 
-  // For example, getting an Invasive Blood Pressure, any arterial compartment pressure can be pulled, i.e. femoral artery (right/left leg)
-  // Since this is an arterial compartment, blood is flowing through this compartment, where as the carina compartment is air flow
+
   pe->GetLogger()->Info(std::stringstream() << "Invasive Blood Pressure : " << pe->GetCompartments().GetLiquidCompartment(pulse::VascularCompartment::Aorta)->GetPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
   
   pe->GetLogger()->Info(std::stringstream() << "RespirationRate : " << pe->GetRespiratorySystem()->GetRespirationRate(FrequencyUnit::Per_min) << "bpm");
